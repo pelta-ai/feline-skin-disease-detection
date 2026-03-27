9cea12488eea178842d564b839dfa426e3b54fd4
anishanup <anishanup26@gmail.com>
2026-03-27 00:34:53 -0500

    New models for training on Colab

--- src/classifiers/classifier_factory.py
+++ src/classifiers/classifier_factory.py
@@ -2,14 +2,22 @@
 
 from .mobile_net_v2_classifier import MobileNetV2Classifier
 from .mobile_net_v3_small_classifier import MobileNetV3SmallClassifier
-from .resnet_18_classifier import ResNet18Classifier
+from .resnet_50_classifier import ResNet50Classifier
+from .efficient_net_b0_classifier import EfficientNetB0Classifier
+from .efficient_net_v2_b0_classifier import EfficientNetV2B0Classifier
+from .nasnet_mobile_classifier import NASNetMobileClassifier
+from .convnext_tiny_classifier import ConvNeXtTinyClassifier
 
 
 class ClassifierFactory:
     _registry = {
         "mobilenet_v2": MobileNetV2Classifier,
         "mobilenet_v3_small": MobileNetV3SmallClassifier,
-        "resnet18": ResNet18Classifier,
+        "resnet50": ResNet50Classifier,
+        "efficientnet_b0": EfficientNetB0Classifier,
+        "efficientnet_v2_b0": EfficientNetV2B0Classifier,
+        "nasnet_mobile": NASNetMobileClassifier,
+        "convnext_tiny": ConvNeXtTinyClassifier,
     }
 
     @classmethod
