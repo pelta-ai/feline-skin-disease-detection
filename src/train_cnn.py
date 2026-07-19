0705ed7666dc045eee9948da68456f2e1750db08
anishanup <96807512+anishanup@users.noreply.github.com>
2026-07-18 19:36:44 -0500

    clean up (#21)

--- src/train_cnn.py
+++
@@ -1,8 +0,0 @@
-import os, sys
-
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
-import src.convolutional_neural_network as cnn
-
-cnn_model = cnn.ConvolutionNeuralNetwork(num_epochs=30)
-cnn_model.build_model()
-cnn_model.train()
